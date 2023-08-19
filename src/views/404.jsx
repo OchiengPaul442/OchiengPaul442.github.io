2ece6fb4a1a0dc792de4db8f35de1d7ce005482b
Ochieng Paul <ochiengpaul442@gmail.com>
2023-08-19 20:23:27 +0300

    updated urls

--- src/views/404.jsx
+++ src/views/404.jsx
@@ -9,10 +9,7 @@ const Eror404 = () => {
                 <div className="px-3 py-3 lg:px-5 lg:pl-3">
                     <div className="flex items-center justify-between">
                         <div className="flex items-center justify-start">
-                            <Link
-                                to="/community_box"
-                                className="flex ml-2 md:mr-24"
-                            >
+                            <Link to="/" className="flex ml-2 md:mr-24">
                                 <CommunityIcon
                                     fill="none"
                                     width="40"
@@ -41,7 +38,7 @@ const Eror404 = () => {
                         deleted.
                     </p>
                     <Link
-                        to="/community_box"
+                        to="/"
                         className="inline-block bg-blue-500 text-white px-6 py-3 rounded-full font-semibold hover:bg-blue-600"
                     >
                         Go back home
